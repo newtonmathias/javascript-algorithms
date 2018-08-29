43596b67552b35de64ae83f2b537b9dfaa2e5848
Newton Mathias <newtymathias@gmail.com>
2018-08-29 22:30:57 +0300

    update

--- radian/degreeToRadian.js
+++ radian/degreeToRadian.js
@@ -1,1 +1,6 @@
-
+function degreeToRadian(degree) {
+    return degree*Math.PI / 180;
+  }
+  
+  console.log(degreeToRadian(5));
+  
--- radian/radianToDegree.js
+++ radian/radianToDegree.js
@@ -1,1 +1,5 @@
+function radianToDegree(radian) {
+  return radian * (180 / Math.PI);
+}
 
+console.log(radianToDegree(0.08726646259971647));
